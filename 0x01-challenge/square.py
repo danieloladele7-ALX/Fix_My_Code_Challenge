e8f933e2e8d2a280b07bede4fa91738b25fdc13c
Oladele Daniel <danieloladele7@gmail.com>
2023-07-19 02:32:57 +0200

    task 2 uploaded

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
+""" 
+square class
+"""
 class square():
+    """ Documentation """
     
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """ Documentation """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,9 +20,11 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Documentation """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Documentation """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
